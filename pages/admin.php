f373e51a50d11ffe87c53fd73dccb5a73daaf8b1
plectus24 <168301948+plectus24@users.noreply.github.com>
2024-07-02 17:17:57 +0200

    Refactor pages and script file
    Added pages into a seperate folder, initalized admin login page

---
+++ pages/admin.php
@@ -0,0 +1,12 @@
+En reparations
+
+<a href="../index.html">Page d'Accueille</a>
+
+<div id="login">
+    <h1>Login</h1>
+    <form action="#" target="#">
+        <label for="username">Nom d'utilisateur<input type="text" name="username" id="username"></label>
+        <label for="password">Password<input type="password"></label>
+        <button type="submit">Enter</button>
+    </form>
+</div>
